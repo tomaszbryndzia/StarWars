a577ff23ce84d32cdb11e0215f9166d41f943967
Tomasz Bryndzia <tomasz.bryndzia@infopark.de>
2020-04-25 18:23:12 +0200

    css loader webpack + styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/index.css";
 
 ReactDOM.render(
   <Provider store={configureStore()}>
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
         },
       },
       {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.(png|jp(e*)g|svg)$/,
         use: [
           {
