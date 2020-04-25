6dfb5060c2c1e0e84cd3615c170591d58007ef02
Tomasz Bryndzia <tomasz.bryndzia@infopark.de>
2020-04-25 13:47:06 +0200

    action rename and import

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { connect } from "react-redux";
 
+import { simpleAction } from "./actions/simpleAction";
+
 // import Main from "./components/Main";
 // import Character from "./components/charDetails/Character";
 
 class App extends Component {
+  simpleAction = (event) => {
+    this.props.simpleAction();
+  };
+
   render() {
     return (
       <Router>
@@ -15,8 +21,9 @@ class App extends Component {
               <div>test</div>
             </Route>
             <Route path="/">
-              <button>Test redux action</button>
+              <button onClick={this.simpleAction}>Test redux action</button>
               <div>cos</div>
+              <pre>{JSON.stringify(this.props)}</pre>
             </Route>
           </Switch>
         </div>
