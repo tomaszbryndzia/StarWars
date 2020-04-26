cdc7c0f9bf0c81884e55faa97ddb006f8a63c099
Tomasz Bryndzia <tomasz.bryndzia@infopark.de>
2020-04-26 17:32:43 +0200

    webpack get issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+var path = require("path");
 
 module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "/",
+  },
   module: {
     rules: [
       {
@@ -9,6 +16,7 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
+            presets: ["@babel/preset-react", ["@babel/preset-env"]],
             plugins: [
               "@babel/plugin-syntax-dynamic-import",
               ["@babel/plugin-proposal-decorators", { legacy: true }],
@@ -42,6 +50,9 @@ module.exports = {
         ],
       },
     ],
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebPackPlugin({
